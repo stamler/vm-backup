e886a6b6e228a2750fb0597efd5f06b46ba30ed9
Dean Stamler <dean@stamler.ca>
2018-05-08 12:09:40 -0400

    Simplify install script

--- README.md
+++ README.md
@@ -5,12 +5,9 @@ Backup live qemu-kvm VMs running on CentOS 7/RHEL via rsync
 
 ```
 sudo su -
-cd ~
-wget https://github.com/stamler/vm-backup/archive/master.zip -O temp.zip
-unzip temp.zip && rm -rf temp.zip
-cd ./vm-backup-master
-chmod 700 ./*.sh
-./install.sh
+wget https://github.com/stamler/vm-backup/archive/master.zip -O ~/temp.zip
+unzip ~/temp.zip && rm -rf ~/temp.zip
+~/vm-backup-master/install.sh
 ```
 
 # Confirm Installation
--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp vm-backup.sh /var/lib/libvirt/images/ &&
-cp vm-backup.service /usr/lib/systemd/system/vm-backup.service &&
-cp vm-backup.timer /usr/lib/systemd/system/vm-backup.timer &&
+cp ~/vm-backup-master/vm-backup.sh /var/lib/libvirt/images/ &&
+cp ~/vm-backup-master/vm-backup.service /usr/lib/systemd/system/vm-backup.service &&
+cp ~/vm-backup-master/vm-backup.timer /usr/lib/systemd/system/vm-backup.timer &&
 rm -rf ~/vm-backup-master
 systemctl enable vm-backup.timer &&
 systemctl start vm-backup.timer
